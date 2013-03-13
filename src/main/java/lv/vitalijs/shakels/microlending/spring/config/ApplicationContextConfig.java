dc356937bbf69e43b26eb44fd639d9ee43f2600a
vitalijs <vitalijs.shakels@gmail.com>
2013-03-13 21:15:27 +0200

    Fixing spring issues (1)

--- src/main/java/lv/vitalijs/shakels/microlending/spring/config/ApplicationContextConfig.java
+++ src/main/java/lv/vitalijs/shakels/microlending/spring/config/ApplicationContextConfig.java
@@ -2,11 +2,9 @@ package lv.vitalijs.shakels.microlending.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan({"lv.vitalijs.shakels.microlending.facade", "lv.vitalijs.shakels.microlending.services"})
+@ComponentScan({"lv.vitalijs.shakels.microlending.facade", "lv.vitalijs.shakels.microlending.services, lv.vitalijs.shakels.microlending.repositories"})
 public class ApplicationContextConfig {
 
 }
