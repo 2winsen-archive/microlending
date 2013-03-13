5a3bb48b16b7e4d5c55bedfa20cfea97a70c4c3a
vitalijs <vitalijs.shakels@gmail.com>
2013-03-13 21:27:45 +0200

    Configured persistence

--- src/main/java/lv/vitalijs/shakels/microlending/spring/config/ApplicationContextConfig.java
+++ src/main/java/lv/vitalijs/shakels/microlending/spring/config/ApplicationContextConfig.java
@@ -1,10 +1,17 @@
 package lv.vitalijs.shakels.microlending.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"lv.vitalijs.shakels.microlending.facade", "lv.vitalijs.shakels.microlending.services, lv.vitalijs.shakels.microlending.repositories"})
 public class ApplicationContextConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 }
