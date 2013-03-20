4320a208b118bbb7e108dcb03673336546e13aea
Vitalijs Sakels <vitalijs.sakels@accenture.com>
2013-03-20 12:58:35 +0200

    Added acceptance selenium test

--- src/test/java/lv/vitalijs/shakels/microlending/spring/config/ApplicationContextTestConfig.java
+++ src/test/java/lv/vitalijs/shakels/microlending/spring/config/ApplicationContextTestConfig.java
@@ -1,8 +1,5 @@
 package lv.vitalijs.shakels.microlending.spring.config;
 
-import lv.vitalijs.shakels.microlending.repositories.LoanRepository;
-import lv.vitalijs.shakels.microlending.stubs.LoanRepositoryStub;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -13,11 +10,6 @@ public class ApplicationContextTestConfig {
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
-	}
-
-	@Bean
-	public static LoanRepository getLoanRepository() {
-		return new LoanRepositoryStub();
 	}
 
 }
